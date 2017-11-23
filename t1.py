25e10ade0477dcdaa75919c23b2e8047bb4ce12c
facingwaller <facingwaller@gmail.com>
2017-11-23 17:55:26 +0800

    1

--- t1.py
+++ t1.py
@@ -7,7 +7,8 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 #784表示一个图片的像素总数
 #表示一个输入源，二维组，第一维度不知道，第二维度是784.
 x = tf.placeholder("float", [None, 784])
-
+# 一个Variable代表一个可修改的张量，存在在TensorFlow的用于描述交互性操作的图中。
+# 它们可以用于计算输入值，也可以在计算中被修改
 W = tf.Variable(tf.zeros([784,10]))
 b = tf.Variable(tf.zeros([10]))
 
