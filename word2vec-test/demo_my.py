8bd6d09ad42f13f3e8c318dd5a5871ab1492ecb1
facingwaller <facingwaller@gmail.com>
2017-12-13 00:22:12 +0800

    train eng model

---
+++ word2vec-test/demo_my.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from gensim.models import word2vec
+from gensim import models
+import logging
+
+def main():
+	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+	model = models.Word2Vec.load('med250.model.bin')
+
+
+	while True:
+		try:
+			print(model["测试"])
+			query = input()
+			q_list = query.split()
+
+			if len(q_list) == 1:
+				print("相似词前 100 排序")
+				res = model.most_similar(q_list[0],topn = 10)
+				for item in res:
+					print(item[0]+","+str(item[1]))
+
+			else :
+				print("计算 Cosine 相似度")
+				res = model.similarity(q_list[0],q_list[1])
+				print(res)
+			print("----------------------------")
+		except Exception as e:
+			print(repr(e))
+
+if __name__ == "__main__":
+	main()
