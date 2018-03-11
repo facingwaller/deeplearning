a0fd814b07e2b46ed5a5914c23cd88d9f00193e7
facingwaller <facingwaller@gmail.com>
2018-03-11 11:10:56 +0800

    同步111

---
+++ lib/logistics_helper.py
@@ -0,0 +1,98 @@
+from lib.ct import ct
+from lib.config import config
+import os
+import gzip
+import gc
+import math
+import numpy as np
+
+
+# 1 装载数据
+
+class logistics_helper:
+    def extract_line(self, f1_l):
+
+        f1_ls = str(f1_l).split('\t')
+        step = f1_ls[0]
+        model = f1_ls[1]
+        global_index = f1_ls[2]
+        question = f1_ls[3]
+        ds = f1_ls[4:]
+        ts = []
+        for item in ds:
+            index = int(str(item).split('_')[0])
+            right = str(item).split('_')[1] == '1'
+            if right:
+                right1 = [1,0]
+            else:
+                right1 = [0, 1]
+            score = float(str(item).split('_')[2])
+            relation = str(item).split('_')[3]
+
+            z_score = ct.get_zi_flag_score_ps(question,relation)
+
+            t1 = (index, right1, score, relation,right,z_score)
+            ts.append(t1)
+        t1 = (global_index, question, ts, step, model)
+        return t1
+
+    def __init__(self,f1='../data/nlpcc2016/8-logistics/logistics-2018-03-10.txt_bak.txt'):
+        self.all_datas = []
+        # f1 = '../data/nlpcc2016/8-logistics/logistics-2018-03-10.txt_bak.txt'
+        f1s = ct.file_read_all_lines_strip(f1)
+        self.train_data = []
+        self.test_data = []
+        for f1_l in f1s:
+            if str(f1_l).__contains__('\tvalid\t'):
+                self.train_data.append(self.extract_line(f1_l))
+            else:
+                self.test_data.append(self.extract_line(f1_l))
+
+        print('init ok')
+
+    def batch_iter(self, data, batch_size):
+        total = len(data)
+        shuffle_indices = np.random.permutation(np.arange(total))  # 打乱样本下标
+
+        info1 = "q total:%d ; epohches-size:%s " % (total, len(data) // batch_size)
+        ct.print(info1, 'info')
+        x_new = []
+        y_new = []
+        z_new = []
+
+        for list_index in range(total):
+            data_current = data[shuffle_indices[list_index]]
+            for ts in data_current[2]:
+                x_new.append(data_current[1])
+                # t1 = (index, right1, score, relation,right,z_score)
+                y_new.append((ts[2],ts[5]))  # 继续遍历
+                z_new.append(ts[1])
+                # ts
+                # t1 = (index, right1, score, relation,right)
+
+
+
+            if list_index % batch_size == 0 and list_index != 0:
+                x_return = x_new.copy()  # 问题
+                y_return = y_new.copy()  # 数据
+                z_return = z_new.copy()  # 标签
+                x_new.clear()
+                y_new.clear()
+                z_new.clear()
+                yield np.array(x_return), np.array(y_return), np.array(z_return)
+
+    def c2(self):
+        print(2)
+
+if __name__ == "__main__":
+    batch_size = 2
+    lh = logistics_helper()
+    gc1 = lh.batch_iter(lh.train_data, batch_size)
+    # for _ in range(n_batch):
+    for gc2 in gc1:
+        x1 = gc2[1]
+        y1 = gc2[2]
+        x1 = x1.reshape(-1, 2)
+        y1 = y1.reshape(-1, 2)
+        print(x1)
+        print(y1)
--- test/t_logistic.py
+++ test/t_logistic.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import numpy as np
-import random
+
+from lib.logistics_helper import logistics_helper
+from lib.ct import ct
 
 # X_SIZE = 784
 # Y_SIZE = 10
@@ -10,7 +12,7 @@ Y_SIZE = 2
 
 # 改成 2 位 ， 2分类
 
-# 大于0.8 的就是
+
 x1 = []
 y1 = []
 
@@ -23,9 +25,7 @@ for i in range(8):
 x1 = np.array(x1)
 y1 = np.array(y1)
 
-from tensorflow.examples.tutorials import mnist
-
-mnist = mnist.input_data.read_data_sets('./data/mnist', one_hot=True)
+# mnist = mnist.input_data.read_data_sets('./data/mnist', one_hot=True)
 
 x = tf.placeholder(dtype=tf.float32, shape=[None, X_SIZE], name='input')  # 784 个  像素
 y = tf.placeholder(dtype=tf.float32, shape=[None, Y_SIZE], name='output')  # 10 分类
@@ -37,58 +37,63 @@ logits = tf.add(tf.matmul(x, w), b)
 
 accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1)), tf.float32))
 
-lr = 0.01
+lr = 0.001
 epochs = 100
-batch_size = 2
+batch_size = 10
+f1='../data/nlpcc2016/8-logistics/logistics-2018-03-10.txt_bak.txt'
+lh = logistics_helper(f1)
+
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y), name='loss')
 optimizer = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
+
+ct.print("lr %s size %s f1= %s"%(lr,batch_size,f1))
+
+def run_step(data, model):
+    # if epoch % 10 == 0:
+    total_loss = 0.0
+    total_acc = 0.0
+    total = 0
+    gc_valid = lh.batch_iter(data, batch_size)
+    for gc_valid_item in gc_valid:
+        total += 1
+        x1 = gc_valid_item[1]
+        y1 = gc_valid_item[2]
+        x1 = x1.reshape(-1, 2)
+        y1 = y1.reshape(-1, 2)
+        val_loss, val_acc,w1,b1 = sess.run([loss, accuracy,w,b],
+                                     # feed_dict={x: mnist.validation.images, y: mnist.validation.labels}
+                                     feed_dict={x: x1, y: y1}
+                                     )
+        if val_acc==0:
+            ct.print(gc_valid_item[0],'error')
+        total_loss += val_loss
+        total_acc += val_acc
+
+    ct.print('model %s epoch %s train loss = %s,train acc %s,the val loss=%s,val acc %s' %
+             (model, epoch, train_loss, train_acc, total_loss / total, total_acc / total), model)
+    ct.print("w1 %s"%(w1))
+    ct.print("b1 %s"%(w1))
+
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     # writer = tf.summary.FileWriter('graphs/logistic_reg', sess.graph)
-    n_batch = mnist.train.num_examples // batch_size
+    # n_batch = logistics_helper. # mnist.train.num_examples // batch_size # 取整除 - 返回商的整数部分
+
     for epoch in range(epochs):
         train_loss = 0
         train_acc = 0
-        for _ in range(n_batch):
-            z1, z2 = mnist.train.next_batch(batch_size)
-            # print(type(z1))
-            # print(type(z1[0]))
-            # print(type(z2))
-            # xs, ys = mnist.train.next_batch(batch_size)
-            xs = x1
-            ys = y1
+        # gc1 = lh.batch_iter(lh.train_data,batch_size)
+        # for _ in range(n_batch):
+        # for gc1_item in gc1:
+        #     x1 = gc1_item[1].reshape(-1,2)
+        #     y1 = gc1_item[2].reshape(-1,2)
+        #     _, batch_loss, batch_acc = sess.run([optimizer, loss, accuracy], feed_dict={x: x1, y: y1})
+        #     train_loss += batch_loss
+        #     train_acc += batch_acc
+        run_step(lh.train_data, 'train')
+        if epoch % (epochs // 10) == 0 and epoch != 0:
+            run_step(lh.train_data, 'valid')
+        if epoch % (epochs // 20) == 0 and epoch != 0:
+            run_step(lh.test_data, 'test')
 
-            # batch_size = 2
-            # wei_du = 2
-            # max_document_length
-            # word_d
-            #         x_train = x_train.reshape((batch_size, max_document_length, word_d))
-            # print(x1.shape)
-            x1 = x1.reshape(-1,2)
-            y1 = y1.reshape(-1,2)
-            # print(x1.shape)
-            # x_image = tf.reshape(x, [-1, 28, 28, 1])
-            # print('......................')
-            # print(x1)
-            # print(y1)
-            _, batch_loss, batch_acc = sess.run([optimizer, loss, accuracy], feed_dict={x: x1, y: y1})
-            train_loss += batch_loss
-            train_acc += batch_acc
-
-        if epoch % 10 == 0:
-            val_loss, val_acc = sess.run([loss, accuracy],
-                                         # feed_dict={x: mnist.validation.images, y: mnist.validation.labels}
-                                         feed_dict={x: x1, y: y1}
-                                         )
-
-            print('epoch {} train loss = {},train acc {},the val loss={},val acc {}' \
-                  .format(epoch, train_loss / n_batch, train_acc / n_batch, val_loss, val_acc))
-
-    test_loss, test_acc = sess.run([loss, accuracy],
-                                   # feed_dict={x: mnist.test.images, y: mnist.test.labels}
-                                   feed_dict={x: x1, y: y1}
-                                   )
-
-    print('the model on test loss is {},the accuracy is {}'.format(test_loss, test_acc))
-    # writer.close()
