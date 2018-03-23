725cb24ac3398fe450d36e86088ba1e3cd37f087
facingwaller <facingwaller@gmail.com>
2018-03-23 15:26:30 +0800

    1

--- lib/config.py
+++ lib/config.py
@@ -102,7 +102,7 @@ elif testid == 'cc_debug':
     # 使用属性的模式做训练和测试
     # 1 num 限制数量 2 special 指定 3 no 非训练模式 4 maybe 模糊属性的单独处理
     skip_threshold = 0.02
-    t_relation_num = 4358  # 重要！这个指示了训练的关系个数
+    t_relation_num = 1000  # 重要！这个指示了训练的关系个数 4358
     # 分割训练和测试 数据集的时候 使用正式的划分（严格区分训练和测试），
     # 而非模拟测试的。 之前是混合在一起
     real_split_train_test = True
@@ -123,7 +123,7 @@ elif testid == 'cc_debug':
     pool_mode = 'additional'
 
     # 模型恢复
-    restore_model = True
+    restore_model = False
     restore_path = \
         r'F:\PycharmProjects\dl2\deeplearning\QA_GAN\runs\2018_03_22_11_55_32_one_day\checkpoints\step=1_epoches=g_index=0\model.ckpt-1'
     #
