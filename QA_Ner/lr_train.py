bc782a953e15c11ef7897749325b65629cade3d9
facingwaller <facingwaller@gmail.com>
2018-04-10 16:05:30 +0800

    同步4.回归NER

---
+++ QA_Ner/lr_train.py
@@ -0,0 +1,164 @@
+# 改成线性回归先用着
+#
+import tensorflow as tf
+import numpy as np
+
+from lib.lr_helper import lr_helper
+from lib.ct import ct
+
+# X_SIZE = 784
+# Y_SIZE = 10
+
+X_SIZE = 2  # len IDF 之后加 SCORE
+Y_SIZE = 2
+
+# 改成 2 位 ， 2分类
+lr = 0.01  # 0.001
+epochs = 100
+batch_size = 10
+# 0_1_0.898056_类型  INDEX 是否正确 分值/IDF 实体
+f1 = '../data/nlpcc2016/6-answer/q.rdf.ms.re.top_3.v1-20180403-1.txt'
+# f1 = '../data/nlpcc2016/8-logistics/logistics-2018-03-20.txt_bak.txt'
+#
+lh = lr_helper(f1)
+
+# ----------------------
+
+x = tf.placeholder(dtype=tf.float32, shape=[None, X_SIZE], name='input')  # 784 个  像素
+y = tf.placeholder(dtype=tf.float32, shape=[None, Y_SIZE], name='output')  # 10 分类
+right_index = tf.placeholder(dtype=tf.int32, name='right_index')  # 正确的分类的index
+# w = tf.Variable(tf.random_normal([X_SIZE, Y_SIZE], seed=1, dtype=tf.float32), name='weights',trainable=True)
+# tf.fill([2,3], 9)
+w = tf.Variable(tf.random_uniform(shape=[X_SIZE, Y_SIZE], minval=0, maxval=2, dtype=tf.float32), name='weights',
+                trainable=True)
+# w = tf.Variable([[ 1.0],[1.0]], name='weights',trainable=True)
+b = tf.Variable(tf.random_normal([1, Y_SIZE], dtype=tf.float32), name='bias', trainable=True)
+
+z = tf.add(tf.matmul(x, w), b)  # 得分
+z1 = tf.reduce_mean(z, 1)  # 取均值
+z_max = tf.reduce_max(z1)  # 取出最大的项
+# # 正确分数
+z_right = tf.gather(z1, right_index)
+# loss = tf.maximum(0.0, tf.reduce_max(z_right) - z_max)
+# loss = tf.multiply(z_max - tf.reduce_max(z_right),10000)  # loss = 最大者 与 正确答案的差距
+loss = z_max - tf.reduce_max(z_right)  # loss = 最大者 与 正确答案的差距
+optimizer = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
+accuracy = tf.equal(loss, 0)
+
+test_x = tf.placeholder(dtype=tf.float32, shape=[None, X_SIZE], name='test_input')  # 784 个  像素
+test_y = tf.placeholder(dtype=tf.float32, shape=[None, Y_SIZE], name='test_output')  # 10 分类
+test_right_index = tf.placeholder(dtype=tf.int32, name='test_right_index')  # 正确的分类的index
+test_z = tf.add(tf.matmul(test_x, w), b)  # 得分
+test_z1 = tf.reduce_mean(test_z, 1)  # 取均值
+test_z_max = tf.reduce_max(test_z1)  # 取出最大的项
+# # 正确分数
+test_z_right = tf.gather(test_z1, test_right_index)
+# loss = tf.maximum(0.0, tf.reduce_max(z_right) - z_max)
+# loss = tf.multiply(z_max - tf.reduce_max(z_right),10000)  # loss = 最大者 与 正确答案的差距
+test_loss = test_z_max - tf.reduce_max(test_z_right)  # loss = 最大者 与 正确答案的差距
+test_accuracy = tf.equal(test_loss, 0)
+
+ct.print("lr %s size %s f1= %s" % (lr, batch_size, f1))
+
+
+def run_step1(data, model):
+    total_loss = 0.0
+    total_acc = 0.0
+    total = 0
+    gc_valid = lh.batch_iter(data, batch_size)
+    error_count = 0
+    right_count = 0
+
+    for gc_valid_item in gc_valid:
+        total += 1
+        x1 = gc_valid_item[1]
+        y1 = gc_valid_item[2]
+        p1 = gc_valid_item[3]
+        r_i = gc_valid_item[4]
+        x1 = x1.reshape(-1, 2)
+        y1 = y1.reshape(-1, 2)
+
+        _z, _z1, _z_max, _z_right, _loss, _w, _b, _accuracy, _optimizer = \
+            sess.run([z, z1, z_max, z_right, loss, w, b, accuracy, optimizer],
+                     feed_dict={x: x1, y: y1, right_index: r_i})
+
+        ct.print(_w, 'w')
+        ct.print(_b, 'b')
+        if _accuracy:
+            right_count += 1
+        else:
+            error_count += 1
+            ct.print(gc_valid_item[1], 'error')
+
+        # print(_loss)
+        total_loss += _loss
+        total_acc += _accuracy
+        if total %1000 == 0:
+            print(total/1000)
+    ct.print(
+        'model %s epoch %s   loss = %s,  acc = %s error_count %s right_count %s total:%s' %
+        (model, epoch, total_loss / total, right_count / total, error_count, right_count, total),
+        'debug')
+    ct.print(_w, 'w')
+    ct.print(_b, 'b')
+    return w, b
+
+
+def test_step1(data, model):
+    total_loss = 0.0
+    total_acc = 0.0
+    total = 0
+    gc_valid = lh.batch_iter(data, batch_size)
+    error_count = 0
+    right_count = 0
+
+    for gc_valid_item in gc_valid:
+        total += 1
+        x1 = gc_valid_item[1]
+        y1 = gc_valid_item[2]
+        p1 = gc_valid_item[3]
+        r_i = gc_valid_item[4]
+        x1 = x1.reshape(-1, 2)
+        y1 = y1.reshape(-1, 2)
+
+        _z, _z1, _z_max, _z_right, _loss, _w, _b, _accuracy = \
+            sess.run([test_z, test_z1, test_z_max, test_z_right, test_loss, w, b, test_accuracy],
+                     feed_dict={test_x: x1, test_y: y1, test_right_index: r_i})
+
+        ct.print(_w, 'w')
+        ct.print(_b, 'b')
+        if _accuracy:
+
+            right_count += 1
+        else:
+            error_count += 1
+        # print(_loss)
+        total_loss += _loss
+        total_acc += _accuracy
+    ct.print(
+        'model %s epoch %s   loss = %s,  acc = %s error_count %s right_count %s total:%s' %
+        (model, epoch, total_loss / total, right_count / total, error_count, right_count, total),
+        'debug')
+    ct.print(_w, 'w')
+    ct.print(_b, 'b')
+    return w, b
+
+
+ct.print("train baseline :%s" % lh.baseline(lh.train_data), 'debug')
+ct.print("test baseline :%s" % lh.baseline(lh.test_data), 'debug')
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    # writer = tf.summary.FileWriter('graphs/logistic_reg', sess.graph)
+    # n_batch = logistics_helper. # mnist.train.num_examples // batch_size # 取整除 - 返回商的整数部分
+
+    for epoch in range(epochs):
+        train_loss = 0
+        train_acc = 0
+
+        w1, b1 = run_step1(lh.train_data, 'train')
+        # if epoch % (epochs // 10) == 0 and epoch != 0:
+        # run_step(lh.train_data, 'valid')
+        test_step1(lh.test_data, 'test')
+        if epoch % (epochs // 10) == 0 and epoch != 0:
+            ct.print("w1=%s b1=%s" % (w1, b1))
---
+++ lib/lr_helper.py
@@ -0,0 +1,153 @@
+from lib.ct import ct
+from lib.config import config
+import os
+import gzip
+import gc
+import math
+import numpy as np
+
+
+# 1 装载数据
+
+class lr_helper:
+    def extract_line(self, f1_l):
+
+        f1_ls = str(f1_l).split('\t')
+        split_word = '____'
+
+        step = f1_ls[0]
+        model = f1_ls[1]
+        global_index = f1_ls[2]
+        question = f1_ls[3]
+        ds = f1_ls[8:]
+        ts = []
+        for item in ds:
+            # 第九行开始
+            try:
+                index = int(str(item).split(split_word)[0])
+            except Exception as e1:
+                print(e1)
+            right = str(item).split(split_word)[1] == '1'
+            if right:
+                right1 = [1, 0]
+            else:
+                right1 = [0, 1]
+            score = float(str(item).split(split_word)[2])
+            relation = str(item).split(split_word)[3]
+
+            # z_score = ct.get_zi_flag_score_ps(question, relation)
+            z_score = len(relation)  # ct.math2(question, relation)
+
+            t1 = (index, right1, score, relation, right, z_score)
+            ts.append(t1)
+        t1 = (global_index, question, ts, step, model)
+        return t1
+
+    def __init__(self, f1='',
+                 f2='../data/nlpcc2016/8-logistics/logistics-2018-03-10.txt_bak.txt'):
+        self.all_datas = []
+        # f1 = '../data/nlpcc2016/8-logistics/logistics-2018-03-10.txt_bak.txt'
+        f1s = ct.file_read_all_lines_strip(f1)
+        self.train_data = []
+        self.test_data = []
+        index = -1
+        for f1_l in f1s:
+            index += 1
+            need_skip =False
+            if str(f1_l).__contains__('NULL'):
+                need_skip = True
+            if str(f1_l).__contains__('####'):
+                need_skip = True
+            if str(f1_l).__contains__('@@@@@@'):
+                 need_skip=True
+
+            if need_skip:
+                continue
+
+            if index < config.cc_par('real_split_train_test_skip'):  # <= int(len(f1s)*0.8):
+                # if str(f1_l).__contains__('\tvalid\t'):
+                self.train_data.append(self.extract_line(f1_l))
+            else:
+                self.test_data.append(self.extract_line(f1_l))
+
+        print('init ok')
+
+    def baseline(self, data):
+        total = len(data)
+        # shuffle_indices = np.random.permutation(np.arange(total))  # 打乱样本下标
+        rith_answer = 0
+        right_index = 0
+        rith_answer = 0
+        right_index = 0
+        for list_index in range(total):
+            index = -1
+            data_current = data[list_index]
+            # for ts in data_current[2]:
+            #     index += 1
+            # 问题  Z分数 NN得分
+            if int(data_current[2][0][1][0]) == 1:
+                rith_answer += 1
+
+        return rith_answer / total
+
+    def batch_iter(self, data, batch_size):
+        total = len(data)
+        shuffle_indices = np.random.permutation(np.arange(total))  # 打乱样本下标
+
+        info1 = "q total:%d ; epohches-size:%s " % (total, len(data) // batch_size)
+        ct.print(info1, 'info')
+        x_new = []
+        y_new = []
+        z_new = []
+        p_new = []
+
+        rith_answer = 0
+        right_index = 0
+        for list_index in range(total):
+            index = -1
+            data_current = data[shuffle_indices[list_index]]
+            for ts in data_current[2]:
+                index += 1
+                x_new.append(data_current[1])
+                # t1 = (index, right1, score, relation,right,z_score)
+                y_new.append((ts[2], ts[5]))  # 继续遍历
+                z_new.append(ts[1])
+                p_new.append(ts[3])
+                # ts
+
+                # 问题  Z分数 NN得分
+                if int(ts[1][0]) == 1:
+                    right_index = index
+                    rith_answer += 1
+                msg = "%s\t%s\t%s\t%s\t%s" % (data_current[1], ts[5], ts[2], ts[3], ts[4])
+                ct.print(msg, 'debug1')
+
+            # if list_index % batch_size == 0 and list_index != 0:
+            x_return = x_new.copy()  # 问题
+            y_return = y_new.copy()  # 数据
+            z_return = z_new.copy()  # 标签
+            p_return = p_new.copy()  # 属性
+
+            x_new.clear()
+            y_new.clear()
+            z_new.clear()
+            p_new.clear()
+            yield np.array(x_return), np.array(y_return), np.array(z_return), np.array(p_return), right_index
+
+
+if __name__ == "__main__":
+    batch_size = 1
+    lh = lr_helper(f1='../data/nlpcc2016/6-answer/q.rdf.ms.re.top_3.v1-20180403-1.txt')
+
+    print(lh.baseline(lh.train_data))
+    print(lh.baseline(lh.test_data))
+
+    gc1 = lh.batch_iter(lh.train_data, batch_size)
+    #
+    # for gc2 in gc1:
+    #     x1 = gc2[1]
+    #     y1 = gc2[2]
+    #     x1 = x1.reshape(-1, 2)
+    #     y1 = y1.reshape(-1, 2)
+    #     print(x1)
+    #     print(y1)
